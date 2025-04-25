ba45eaf5c0d5f7408bdf7e983affaf3da39c84d7
ConnorAU <15099385+ConnorAU@users.noreply.github.com>
2025-04-25 10:47:55 +1000

    Add mod.cpp Adding to git for use in remote build workflow

---
+++ mod.cpp
@@ -0,0 +1,7 @@
+name="AnimatePAA";
+overview="Transform your vehicle skins and marker icons from static images to GIF-like animations!";
+tooltipOwned="AnimatePAA";
+action="https://github.com/ConnorAU/A3AnimatePAA";
+
+hideName=0;
+hidePicture=0;
